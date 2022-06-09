8f9778392950b0c38a710fdd6590661208fb046a
Aditya Singh <deviladi00@gmail.com>
2022-06-09 19:44:22 +0530

    Add files via upload

--- transaction.sql
+++ transaction.sql
@@ -1,13 +1,16 @@
 use fms;
-create table personnel( 
+create table transaction( 
 	id int primary key auto_increment,
-	user_id varchar(255),
-    first_name varchar(255),
-    last_name varchar(255), 
-    dob date,
-    gender varchar(255),
-    contact_no varchar(255),
-    email varchar(255),
-    password varchar(255),
+	card_no bigint,
+	user_id varchar(30),
+    card_holder_name varchar(30),
+	acc_no bigint,
+	expiry_date date,
+	card_type varchar(30),
+	trans_date date,
+	trans_details varchar(30),
+	remarks varchar(30),
+	fraud_level varchar(30),
+	blocked int,
 	is_authorized int
 );
