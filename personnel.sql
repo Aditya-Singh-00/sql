deded94ce76b74d8d5a2357d34c8a761a520f79c
Aditya Singh <deviladi00@gmail.com>
2022-06-13 17:00:06 +0530

    updated personnel

--- personnel.sql
+++ personnel.sql
@@ -1,13 +1,14 @@
-use fms;
-create table personnel( 
-	id int primary key auto_increment,
-	user_id varchar(255),
-    first_name varchar(255),
-    last_name varchar(255), 
-    dob date,
-    gender varchar(255),
-    contact_no varchar(255),
-    email varchar(255),
-    password varchar(255),
-	is_authorized int
+CREATE TABLE `fraud_analysis_personnels` (
+  `id` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `contact_no` varchar(10) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `is_authorized` int DEFAULT NULL,
+  `first_answer` varchar(255) DEFAULT NULL,
+  `second_answer` varchar(255) DEFAULT NULL,
+  `third_answer` varchar(255) DEFAULT NULL
 );
