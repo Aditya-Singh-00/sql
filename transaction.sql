85d68ae33b2973557f7edc4914a6ee499a1d6637
Aditya Singh <deviladi00@gmail.com>
2022-06-13 17:00:50 +0530

    updated transactions

--- transaction.sql
+++ transaction.sql
@@ -1,16 +1,13 @@
-use fms;
-create table transaction( 
-	id int primary key auto_increment,
-	card_no bigint,
-	user_id varchar(30),
-    card_holder_name varchar(30),
-	acc_no bigint,
-	expiry_date date,
-	card_type varchar(30),
-	trans_date date,
-	trans_details varchar(30),
-	remarks varchar(30),
-	fraud_level varchar(30),
-	blocked int,
-	is_authorized int
+CREATE TABLE `transactions` (
+  `id` int DEFAULT NULL,
+  `card_no` mediumtext,
+  `user_id` varchar(255) DEFAULT NULL,
+  `card_holder_name` varchar(255) DEFAULT NULL,
+  `card_type` varchar(255) DEFAULT NULL,
+  `account_no` mediumtext,
+  `expiry_date` date DEFAULT NULL,
+  `transaction_date` date DEFAULT NULL,
+  `transaction_details` varchar(255) DEFAULT NULL,
+  `remarks` varchar(255) DEFAULT NULL,
+  `fraud_level` varchar(10) DEFAULT NULL
 );
